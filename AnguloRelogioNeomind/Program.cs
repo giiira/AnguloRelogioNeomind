d688e9567515c1e392039b9fd79d5eeff009afaf
girar <girar@GIRA_PC>
2024-04-06 15:10:44 -0300

    Adicionar arquivos de projeto.

---
+++ AnguloRelogioNeomind/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AnguloRelogio;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float hora = 0;
+        float minuto = 0;
+        float hAngle;
+        float mAngle;
+        float angle;
+
+        InputData(ref hora, ref minuto);
+
+        hAngle = 0.5f * (hora * 60 + minuto);
+        mAngle = 6 * minuto;
+        angle = Math.Abs(hAngle - mAngle);
+        angle = Math.Abs(Math.Min(angle, 360 - angle));
+
+        ShowResult(angle);
+
+        static void InputData(ref float hora, ref float minuto)
+        {
+            Console.WriteLine("Informe o valor da hora desejada: ");
+            hora = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor dos minutos desejados: ");
+            minuto = float.Parse(Console.ReadLine());
+        }
+
+        if (hora > 12)
+        {
+            hora = hora - 12;
+
+        }
+
+        static void ShowResult(float ang)
+        {
+            Console.WriteLine($"O ângulo entre os ponteiros é: {ang:F2}");
+
+        }
+
+
+    }
+}
